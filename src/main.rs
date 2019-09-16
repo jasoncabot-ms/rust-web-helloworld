c494916a93ab9c2c04eae8d19b55c2b1ff3998a6
Jason Cabot <Jason.Cabot@microsoft.com>
2019-09-16 09:40:41 +0100

    Allow binding to custom port defined by environment variable

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use actix_web::{web, App, HttpRequest, HttpServer, Responder};
+use std::env;
 
 fn greet(req: HttpRequest) -> impl Responder {
     let name = req.match_info().get("name").unwrap_or("World");
@@ -6,13 +7,20 @@ fn greet(req: HttpRequest) -> impl Responder {
 }
 
 fn main() {
+    let key = "PORT";
+    let env_value;
+    match env::var(key) {
+        Ok(val) => env_value = val,
+        Err(e) => env_value = "8080".to_string(),
+    }
+
     HttpServer::new(|| {
         App::new()
             .route("/", web::get().to(greet))
             .route("/{name}", web::get().to(greet))
     })
-    .bind("0.0.0.0:8000")
-    .expect("Can not bind to port 8000")
+    .bind(format!("0.0.0.0:{}", env_value))
+    .expect("Can not bind to port")
     .run()
     .unwrap();
 }
